c663df7502405612b605d183ac1a8537be39e38a
Juan Ceballos <juandavi789@hotmail.com>
2025-10-21 14:15:05 -0500

    Deleted unnecessary exception, we are going to use from now two types of exceptions: Business and technical exceptions along side an enum with the message for every type of exception

--- domain/model/src/main/java/co/onclass/model/exceptions/TecnologiaYaExisteException.java
+++
@@ -1,7 +0,0 @@
-package co.onclass.model.exceptions;
-
-public class TecnologiaYaExisteException extends RuntimeException {
-    public TecnologiaYaExisteException(String nombre) {
-        super("La tecnología con el nombre '" + nombre + "' ya existe.");
-    }
-}
