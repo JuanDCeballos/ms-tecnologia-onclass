eb648ec1eea42806ef6fe079a20ff9d655ce0eb9
Juan Ceballos <juandavi789@hotmail.com>
2025-10-23 19:57:47 -0500

    Added new message to enum of exceptions messages

--- domain/model/src/main/java/co/onclass/enums/ExceptionMessages.java
+++ domain/model/src/main/java/co/onclass/enums/ExceptionMessages.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ExceptionMessages {
 
-    TECNOLOGIA_YA_EXISTE(409, "La tecnología ya existe en nuestro sistema.");
+    TECNOLOGIA_YA_EXISTE(409, "La tecnología ya existe en nuestro sistema."),
+    TECNOLOGIA_NO_ENCONTRADA(404, "La tecnología no fue encontrada."),
+    ;
 
     private final int code;
     private final String message;
