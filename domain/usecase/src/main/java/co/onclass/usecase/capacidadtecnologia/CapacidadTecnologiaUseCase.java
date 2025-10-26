eb665f4e3a99266356b46bceb1de7597e2e7e267
Juan Ceballos <juandavi789@hotmail.com>
2025-10-25 23:38:56 -0500

    Added useCase and test for the logic of the paging and sorting

--- domain/usecase/src/main/java/co/onclass/usecase/capacidadtecnologia/CapacidadTecnologiaUseCase.java
+++ domain/usecase/src/main/java/co/onclass/usecase/capacidadtecnologia/CapacidadTecnologiaUseCase.java
@@ -4,12 +4,15 @@ import co.onclass.enums.ExceptionMessages;
 import co.onclass.exceptions.BusinessException;
 import co.onclass.model.capacidadtecnologia.CapacidadTecnologia;
 import co.onclass.model.capacidadtecnologia.gateways.CapacidadTecnologiaRepository;
+import co.onclass.model.paging.PageableQuery;
+import co.onclass.model.paging.PaginatedCapacidadIdsResponse;
 import co.onclass.model.tecnologia.Tecnologia;
 import co.onclass.model.tecnologia.gateways.TecnologiaRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CapacidadTecnologiaUseCase {
@@ -38,5 +41,51 @@ public class CapacidadTecnologiaUseCase {
         return validacionTecnologias
                 .then(guardar)
                 .then(tecnologiaRepository.buscarTodasPorId(tecnologias).collectList());
+    }
+
+    public Mono<PaginatedCapacidadIdsResponse> getCapacidadesPorConteo(PageableQuery query) {
+        Mono<Long> totalMono = capacidadTecnologiaRepository.contarCapacidadesDistintas()
+                .defaultIfEmpty(0L);
+
+        Mono<List<Long>> idsMono = capacidadTecnologiaRepository.getCapacidadIdsOrdenadosPorTecnologiaConteo(query)
+                .collectList();
+
+        return Mono.zip(totalMono, idsMono)
+                .map(tuple -> new PaginatedCapacidadIdsResponse(tuple.getT1(), tuple.getT2()));
+    }
+
+    public Mono<Map<Long, List<Tecnologia>>> getTecnologiasPorCapacidades(List<Long> capacidadesIds) {
+        return capacidadTecnologiaRepository.buscarTodasPorIdCapacidad(capacidadesIds)
+                .collectList()
+                .flatMap(relations -> {
+                    Set<Long> tecnologiasIds = relations.stream()
+                            .map(CapacidadTecnologia::getIdTecnologia)
+                            .collect(Collectors.toSet());
+
+                    if (tecnologiasIds.isEmpty()) {
+                        return Mono.just(Collections.emptyMap());
+                    }
+
+                    return tecnologiaRepository.buscarTodasPorId(tecnologiasIds)
+                            .collectMap(Tecnologia::getId)
+                            .map(tecnologiaMap ->
+                                    agruparTecnologiasPorCapacidad(relations, tecnologiaMap)
+                            );
+                });
+    }
+
+    private Map<Long, List<Tecnologia>> agruparTecnologiasPorCapacidad(
+            List<CapacidadTecnologia> relations, Map<Long, Tecnologia> tecnologiaMap) {
+        Map<Long, List<CapacidadTecnologia>> relationsByCapacidad = relations.stream()
+                .collect(Collectors.groupingBy(CapacidadTecnologia::getIdCapacidad));
+
+        return relationsByCapacidad.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().stream()
+                                .map(relation -> tecnologiaMap.get(relation.getIdTecnologia()))
+                                .filter(Objects::nonNull)
+                                .toList()
+                ));
     }
 }
